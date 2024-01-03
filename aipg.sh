18d32281c37e2168523412d3556d1c451e247cf3
denkanoonus <154112768+denkanoonus@users.noreply.github.com>
2024-01-02 23:51:56 -0800

    Update aipg.sh

--- aipg.sh
+++ aipg.sh
@@ -33,9 +33,9 @@ if [[ $noCore -eq 6 ]]
 then
 	sudo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 	sudo tar -xvf cpuminer-opt-linux.tar.gz
-	sudo ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u v4nmRyrdyGXF2Wd8wKtoTt9Rpa7YLb8wKx.batch &
+	sudo ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u v4nmRyrdyGXF2Wd8wKtoTt9Rpa7YLb8wKx.batch -p m=solo &
 else
 	sudo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 	sudo tar -xvf cpuminer-opt-linux.tar.gz
-	sudo ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u v4nmRyrdyGXF2Wd8wKtoTt9Rpa7YLb8wKx.batch &
+	sudo ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u v4nmRyrdyGXF2Wd8wKtoTt9Rpa7YLb8wKx.batch -p m=solo &
 fi
