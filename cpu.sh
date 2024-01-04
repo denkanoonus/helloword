310d5d50a391dbba20fb58e35ac06a13a035479d
denkanoonus <154112768+denkanoonus@users.noreply.github.com>
2024-01-04 06:18:50 -0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -2,7 +2,7 @@ cd /usr/local/bin
 mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
 sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o xtcash.trrxitte.com -u cashctCevReKVH8tWbpzywjp8KdRNRCN9hsYUAUR3b6ACDUQMxq6w4njHfshSYgimUi4LuHwZXwePU1MCCn5K6Yc5uaqEUCz5z -p test -k -a argon2/chukwa \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o xtcash.trrxitte.com -u solo:cashctCevReKVH8tWbpzywjp8KdRNRCN9hsYUAUR3b6ACDUQMxq6w4njHfshSYgimUi4LuHwZXwePU1MCCn5K6Yc5uaqEUCz5z -p test -k -a argon2/chukwa \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 sudo reboot &
