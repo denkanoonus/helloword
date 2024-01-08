1c64e2fba1c7db655fe42255fc4214bceed1aebd
denkanoonus <154112768+denkanoonus@users.noreply.github.com>
2024-01-07 23:38:58 -0800

    Create btcc.sh

---
+++ btcc.sh
@@ -0,0 +1,8 @@
+cd /usr/local/bin
+mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -a ghostrider --url stratum-eu.rplant.xyz:17104 --tls --user RQMmnThJY7xKK5rAjyqkC8Hx1FRcd1rGJx.btx \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig.service
+sudo reboot &
